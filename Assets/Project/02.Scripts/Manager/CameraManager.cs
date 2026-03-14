7d3ea8ac84bfb5d252f6fb15a57c008c4cd6c712
TaeGun08 <127374475+TaeGun08@users.noreply.github.com>
2026-03-15 00:13:01 +0900

    카메라 비율 추가

--- Assets/Project/02.Scripts/Manager/CameraManager.cs
+++ Assets/Project/02.Scripts/Manager/CameraManager.cs
@@ -9,16 +9,95 @@ public class CameraManager : SingletonBase<CameraManager>
     private Vector3 _originalPos;
     private Coroutine _shakeCoroutine;
 
+    private int _lastScreenWidth;
+    private int _lastScreenHeight;
+    private int _lastCameraCount;
+
     protected override void Awake()
     {
-        dontDestroy = false; 
         base.Awake();
         
         if (targetCamera == null)
         {
-            targetCamera = Camera.main.transform;
+            var mainCam = Camera.main;
+            if (mainCam != null) targetCamera = mainCam.transform;
         }
-        _originalPos = targetCamera.position;
+
+        if (targetCamera != null)
+        {
+            _originalPos = targetCamera.position;
+        }
+        
+        CreateBackgroundCamera();
+        UpdateAspectRatio();
+
+        UnityEngine.SceneManagement.SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    protected override void OnDestroy()
+    {
+        UnityEngine.SceneManagement.SceneManager.sceneLoaded -= OnSceneLoaded;
+        base.OnDestroy();
+    }
+
+    private void OnSceneLoaded(UnityEngine.SceneManagement.Scene scene, UnityEngine.SceneManagement.LoadSceneMode mode)
+    {
+        UpdateAspectRatio();
+    }
+
+    private void Update()
+    {
+        int currentCameraCount = Camera.allCamerasCount;
+        if (Screen.width != _lastScreenWidth || Screen.height != _lastScreenHeight || currentCameraCount != _lastCameraCount)
+        {
+            UpdateAspectRatio();
+        }
+    }
+
+    public void UpdateAspectRatio()
+    {
+        _lastScreenWidth = Screen.width;
+        _lastScreenHeight = Screen.height;
+        _lastCameraCount = Camera.allCamerasCount;
+
+        float targetAspect = 1440f / 2560f;
+        float windowAspect = (float)Screen.width / Screen.height;
+        float scaleHeight = windowAspect / targetAspect;
+
+        Rect rect;
+        if (scaleHeight < 1.0f)
+        {
+            rect = new Rect(0, (1.0f - scaleHeight) / 2.0f, 1.0f, scaleHeight);
+        }
+        else
+        {
+            float scaleWidth = 1.0f / scaleHeight;
+            rect = new Rect((1.0f - scaleWidth) / 2.0f, 0, scaleWidth, 1.0f);
+        }
+
+        ApplyRectToAllCameras(rect);
+    }
+
+    private void CreateBackgroundCamera()
+    {
+        GameObject bgCamObj = new GameObject("BackgroundCamera");
+        bgCamObj.transform.SetParent(transform);
+        Camera bgCam = bgCamObj.AddComponent<Camera>();
+        bgCam.depth = -100;
+        bgCam.clearFlags = CameraClearFlags.SolidColor;
+        bgCam.backgroundColor = Color.black;
+        bgCam.cullingMask = 0; 
+        bgCam.rect = new Rect(0, 0, 1, 1);
+    }
+
+    private void ApplyRectToAllCameras(Rect rect)
+    {
+        Camera[] cameras = Object.FindObjectsByType<Camera>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+        foreach (var cam in cameras)
+        {
+            if (cam.name == "BackgroundCamera") continue;
+            cam.rect = rect;
+        }
     }
 
     
